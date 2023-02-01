8ee35c1c4031b0ec033c38658a98d3bdcf40d4d9
NikolaGadze <85805336+NikolaGadze@users.noreply.github.com>
2023-02-01 18:49:07 +0100

    Prvi commit

---
+++ database/migrations/2023_02_01_172620_create_notifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_from');
+            $table->integer('user_to');
+            $table->text('message');
+            $table->dateTime('datetime');
+            $table->boolean('opened');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
